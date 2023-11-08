ccd95ab363b9bf60ea729f03ebf9612d88e70bfa
mickydss13 <98723132+mickydss13@users.noreply.github.com>
2023-11-08 13:17:41 -0500

    register/login and sends get requests for category buttons

--- server.py
+++ server.py
@@ -1,29 +1,89 @@
-from markupsafe import escape
-import os
-from flask import Flask,request,make_response, render_template,send_from_directory
+import sys
+
+from util.globals import ACCOUNT, TOKEN, AUCTION
+
+from flask import Flask, request, make_response, send_from_directory
+
+from util.login import login
+from util.register import register
+
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    print()
-    resp = make_response(send_from_directory('public','index.html'))
+    print("in-dex")
+    resp = make_response(send_from_directory('public', 'index.html'))
     #add headers
     resp.headers['X-Content-Type-Options'] = 'nosniff'
 
     return resp
+#
+# @app.route("/post-history")
+# def historyHandler():
+
+# {
+#             '_id': self.index,
+#             'username': username,
+#             'password': encrypt_password,
+#         }
+# @app.route("/post_auction_image")
+# def handleImageUpload():
+#     posts = AUCTION.getAllPost()
+#     messageHistory = []
+#     auth_token = request.cookies.get('auth_token')
+# 
+#     for ele in posts:
+#         messageHistory.append({'_id': ele['_id'],
+#                                'username': ele['username'],
+#                                'title': ele['title'],
+#                                'description': ele['description'],
+#                                'likes': ele['likes'],
+#                                'didCurrentUserLike': didCurrentUserLike(POSTS, TOKEN, ele['_id'], auth_token)
+#                                })
+#     resp = make_response(jsonify(messageHistory))
+#     resp.mimetype = 'application/json'
+#     resp.headers['X-Content-Type-Options'] = 'nosniff'
+#     return resp
+#     image = request.form.get('upload')
+#
+#     # Generate a secure filename for the uploaded image.
+#     filename = secure_filename(image.filename)
+#
+#     # Define the path where you want to save the image.
+#     upload_path = 'path/to/your/directory/' + filename
+#
+#     # Save the uploaded image to the specified path.
+#     image.save(upload_path)
+
+
+@app.route("/register", methods=['POST'])
+def handleRegister():
+    print(request.form,file=sys.stderr)
+    username = request.form.get('username_reg')
+    password = request.form.get('password_reg')
+    return register(ACCOUNT,username,password)
+
+@app.route("/login", methods=['POST'])
+def handleLogin():
+    print("handle login")
+    username = request.form.get('username_login')
+    password = request.form.get('password_login')
+    return login(ACCOUNT,TOKEN,username,password)
+
 
 @app.route("/<path:path>")
 def getPage(path):
-    print(path)
     root = '.'
     if not path.__contains__("public"):
         root = 'public'
-    resp = make_response(send_from_directory(root,path))
+    resp = make_response(send_from_directory(root, path))
     resp.headers['X-Content-Type-Options'] = 'nosniff'
     return resp
 
+
 if __name__ == "__main__":
-    app.run('0.0.0.0',8080)
+    app.run('0.0.0.0', 8080)
 
--- util/database/db.py
+++ util/database/db.py
@@ -1,42 +1,102 @@
+import os
+import secrets
+
 from pymongo import MongoClient
 
 CLIENT = 'mongo'
 DATABASE = 'auctionDb'
 
+
 class AuctionDb:
-    def __init__(self,collectionName):
-        self.client = MongoClient(CLIENT)
+    def __init__(self, collectionName):
+        self.client = MongoClient("mongodb://localhost:27017")
         self.db = self.client[DATABASE]
         self.collection = self.db[collectionName]
 
-    def insert_record(self,record):
+    def insert_record(self, record):
         try:
             self.collection.insert_one(record)
         except:
             print("error inserting record")
             return
-        return 
+        return
 
-    def find_one_record(self,record):
+    def find_one_record(self, record):
         return self.collection.find_one(record)
-    
-    def find_all_records(self,record):
+
+    def find_all_records(self, record):
         records = []
         cursor = self.collection.find(record)
         for i in cursor:
             records.append(i)
         return records
-    
+
     def update_record(self, record_to_update, new_value):
         try:
             self.collection.find_one_and_update(record_to_update, {'$set': new_value})
         except:
             print("error updating record")
             return
-    
+
     def delete_record(self, record_to_delete):
         try:
             self.collection.delete_one(record_to_delete)
         except:
             print("error deleting record")
             return
+
+    def findDocument(self, collection, key: str, value):
+        return collection.find_one({key: value})
+
+
+class Auction(AuctionDb):
+    def __init__(self):
+        super().__init__('auctions')
+
+    def generate_auction_id(self, auction_id):
+        token = secrets.token_hex(16)
+        self.collection.update_one({'_id': auction_id}, {'$set': {'auction_token': token}})
+        return token
+
+    def add_item_image(self, auction_token, image_bytes):
+        image_name = auction_token + ".jpg"
+        # image_name = self.generate_auction_id() + ".jpg"
+        file_path = "public/image/auction_images" + image_name
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)
+
+        with open(file_path, 'wb') as file:
+            file.write(image_bytes)
+
+        self.collection.update_one({'auction_token': auction_token}, {'$set': {'image_name': image_name}})
+        return
+
+    def get_all_auctions(self):
+        return self.posts.find({})
+
+class Account(AuctionDb):
+    def __init__(self):
+        super().__init__('accounts')
+
+    def getAccount(self,username:str):
+        return super().findDocument(self.collection, '_id', username)
+
+    def createAccount(self, username: str, passwordHash: bytes):
+        try:
+            self.collection.insert_one({"_id": username, "password": passwordHash})
+            return True
+        except:
+            return False
+
+
+class Token(AuctionDb):
+    def __init__(self):
+        super().__init__('tokens')
+
+    def createToken(self, username: str, token: bytes):
+        try:
+            self.collection.find_one_and_update({"_id": username}, {'$set': {"tokenHash": token}}, upsert=True)
+            return True
+        except:
+            return False
+
+
